671140e3c6aafba08b1cf7e6134685fd4b475769
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-14 14:53:44 +0900

    style : Booklist 구조 수정

--- frontend/src/Pages/Books.js
+++ frontend/src/Pages/Books.js
@@ -11,11 +11,16 @@ import StarBorder from "@mui/icons-material/StarBorder";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 
-import SearchBar from "../Components/SearchBar";
-import "../App.css";
 import FormControl from "@mui/material/FormControl";
 import NativeSelect from "@mui/material/NativeSelect";
 import Box from "@mui/material/Box";
+
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+
+import SearchBar from "../Components/SearchBar";
+import "../App.css";
 
 export default function BookPage() {
   const [open, setOpen] = React.useState(true);
@@ -29,6 +34,22 @@ export default function BookPage() {
       <SearchBar></SearchBar>
       <Container>
         <div class="container">
+          <div class="item">A</div>
+          <div class="item" style={{ float: "right" }}>
+            <FormControl sx={{ minWidth: 120 }}>
+              <NativeSelect
+                defaultValue={"latest"}
+                inputProps={{
+                  name: "category",
+                  id: "uncontrolled-native",
+                }}
+              >
+                <option value={"latest"}>최신순</option>
+                <option value={"star"}>인기순</option>
+                <option value={"difficulty"}>난이도순</option>
+              </NativeSelect>
+            </FormControl>
+          </div>
           <div class="item">
             <List
               sx={{ width: "100%", maxWidth: 250, bgcolor: "background.paper" }}
@@ -88,28 +109,94 @@ export default function BookPage() {
               </Collapse>
             </List>
           </div>
-          <div class="item">
-            <div style={{ float: "right" }}>
-              <FormControl sx={{ minWidth: 120 }}>
-                <NativeSelect
-                  defaultValue={"latest"}
-                  inputProps={{
-                    name: "category",
-                    id: "uncontrolled-native",
-                  }}
-                >
-                  <option value={"latest"}>최신순</option>
-                  <option value={"star"}>인기순</option>
-                  <option value={"difficulty"}>난이도순</option>
-                </NativeSelect>
-              </FormControl>
-            </div>
-            <div>
-              <Paper></Paper>
-            </div>
+          <div className="item">
+            <Paper>
+              <ImageList sx={{ width: 500, height: 450 }}>
+                {itemData.map((item) => (
+                  <ImageListItem key={item.img}>
+                    <img
+                      src={`${item.img}?w=248&fit=crop&auto=format`}
+                      srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                      alt={item.title}
+                      loading="lazy"
+                    />
+                    <ImageListItemBar
+                      title={item.title}
+                      subtitle={<span>by: {item.author}</span>}
+                      position="below"
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            </Paper>
           </div>
+          <div className="item">E</div>
+          <div className="item">F</div>
         </div>
       </Container>
     </div>
   );
 }
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "@nolanissac",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "@hjrc33",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "@katie_wasserman",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "@silverdalex",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+    author: "@shelleypauls",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "@peterlaster",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    author: "@southside_customs",
+  },
+];
