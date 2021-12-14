0ad8a1afe018ce23a967281d815be562556e13ca
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-14 14:33:44 +0900

    feat : 문제집 nav bar

--- frontend/src/Pages/Books.js
+++ frontend/src/Pages/Books.js
@@ -1,10 +1,95 @@
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+
+import SearchBar from "../Components/SearchBar";
+import "../App.css";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+import Box from "@mui/material/Box";
 
 export default function BookPage() {
-  
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <>
-    <h1>Book Page</h1>
-    </>
+    <div>
+      <SearchBar></SearchBar>
+      <Container>
+        <div class="container">
+          <div class="item">
+            <List
+              sx={{ width: "100%", maxWidth: 250, bgcolor: "background.paper" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="즐겨찾기" />
+              </ListItemButton>
+              <ListItemButton onClick={handleClick}>
+                <ListItemText primary="EBS" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="수능완성" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="수능특강" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+              <ListItemButton onClick={handleClick}>
+                <ListItemText primary="교육청" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="4월" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="10월" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+              <ListItemButton onClick={handleClick}>
+                <ListItemText primary="평가원" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="6월" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="9월" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemText primary="수능" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </List>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
-
