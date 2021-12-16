9c36139f35145d7e8fa5355775a397ebccf67c57
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-16 16:41:49 +0900

    chore : book list height 수정

--- frontend/src/Components/BookList.js
+++ frontend/src/Components/BookList.js
@@ -6,7 +6,7 @@ import Paper from "@mui/material/Paper";
 
 const BookList = ({ posts }) => {
   return (
-    <ImageList sx={{ width: "100%", height: 500 }} cols={3} gap={10}>
+    <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={10}>
       {posts.map((item) => (
         <ImageListItem key={item.img}>
           <img
