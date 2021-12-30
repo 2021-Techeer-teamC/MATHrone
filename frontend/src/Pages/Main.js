93bce16af8313ded17d6a90ed26de39f7b655bb9
YujeongLEE <dldbwjd24@ewhain.net>
2021-12-31 01:43:12 +0900

    feat : carousel 골격

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -3,6 +3,9 @@ import "../Assets/styles/components.css"
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 import Header from "../Components/Header";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
@@ -12,13 +15,41 @@ const theme = createTheme();
 
 export default function Main(props) {
   return (
-    <>
-      <Header title="MATHrone" />
-      <NavBar sections={props.sections} />
-      <Footer
-        title="Footer"
-        description="Something here to give the footer a purpose!"
-      />
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <Header title="MATHrone" sections={sections} />
+          <main class="main">
+            <Carousel
+              autoPlay={true}
+              interval={3000} // default = 3000
+              infiniteLoop={true}
+              swipeable={true} // default = true
+              width={"500px"}
+              showStatus={false}
+              showThumbs={false}
+            >
+              <div>
+                <img src="https://images.unsplash.com/photo-1518756131217-31eb79b20e8f" />
+              </div>
+              <div>
+                <img src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25" />
+              </div>
+              <div>
+                <img src="https://images.unsplash.com/photo-1567306301408-9b74779a11af" />
+              </div>
+            </Carousel>
+            <Routes>
+              <Route path="/info" exact element={<InfoPage />} />
+              <Route path="/books" exact element={<Books />} />
+              <Route path="/rank" exact element={<Rank />} />
+            </Routes>
+          </main>
+        </Container>
+        <Footer
+          title="Footer"
+          description="Something here to give the footer a purpose!"
+        />
+      </ThemeProvider>
   );
 }
