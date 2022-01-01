5394285284c5ba9ee25a3369022cece774aed787
장현우 <ashyonu@gmail.com>
2022-01-01 22:47:04 +0900

    fix : add colum notnull annotation
    change colum name

--- backend/src/main/java/mathrone/backend/domain/BookInfoDTO.java
+++ backend/src/main/java/mathrone/backend/domain/BookInfoDTO.java
@@ -1,4 +1,5 @@
 package mathrone.backend.domain;
+import com.sun.istack.NotNull;
 import com.vladmihalcea.hibernate.type.array.IntArrayType;
 import lombok.*;
 import org.hibernate.annotations.Type;
@@ -20,25 +21,30 @@ public class BookInfoDTO {
     private int workbook_id;
 
     @Column(name = "title")
+    @NotNull
     private String title;
 
     @Column(name = "publisher")
+    @NotNull
     private String publisher;
 
     @Column(name = "profile_img")
+    @NotNull
     private String profile_img;
 
-    @Column(name = "context")
-    private String context;
+    @Column(name = "content")
+    @NotNull
+    private String content;
 
     @Column(name = "type")
+    @NotNull
     private String type;
 
     @Column(name = "year")
-    private Integer year;
+    private Short year;
 
     @Column(name = "month" )
-    private Integer month;
+    private Short month;
 
     @Column(name="chapter_id")
     @Type(type = "int-array")
