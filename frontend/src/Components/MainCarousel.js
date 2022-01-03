95e8ebf4a7fdde35f7c2a7cbf010b8d88456195b
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-03 23:07:23 +0900

    feat : Carousel link

--- frontend/src/Components/MainCarousel.js
+++ frontend/src/Components/MainCarousel.js
@@ -8,32 +8,37 @@ const MainCarousel = ({ posts }) => {
   const preventDefault = (event) => event.preventDefault();
 
   return (
-    <Carousel
-      autoPlay={true}
-      interval={3000} // default = 3000
-      infiniteLoop={true}
-      swipeable={true} // default = true
-      width={"100%"}
-      showStatus={false}
-      showThumbs={false}
-      dynamicHeight={false}
-    >
-      {posts.map((item) => (
-        <div>
-          <Link
-            href="#"
-            underline="hover"
-            style={{ position: "absolute", transform: "translate(40%, 1200%)" }}
-          >
-            {item.year + " " + item.title + " 바로 가기"}
-          </Link>
-          <YearLabel>{item.year}</YearLabel>
-          <TitleLabel>{item.title}</TitleLabel>
-          <ContentLabel>{item.content}</ContentLabel>
-          <img src={item.img} />
-        </div>
-      ))}
-    </Carousel>
+    <div style={{ paddingBottom: "60px", borderBottom: "1px solid #E4E4E4" }}>
+      <Carousel
+        autoPlay={true}
+        interval={3000} // default = 3000
+        infiniteLoop={true}
+        swipeable={true} // default = true
+        width={"100%"}
+        showStatus={false}
+        showThumbs={false}
+        dynamicHeight={false}
+      >
+        {posts.map((item) => (
+          <div>
+            <Link
+              href="#"
+              underline="hover"
+              style={{
+                position: "absolute",
+                transform: "translate(40%, 1200%)",
+              }}
+            >
+              {item.year + " " + item.title + " 바로 가기"}
+            </Link>
+            <YearLabel>{item.year}</YearLabel>
+            <TitleLabel>{item.title}</TitleLabel>
+            <ContentLabel>{item.content}</ContentLabel>
+            <img src={item.img} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
