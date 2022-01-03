5caef4e4ff62d5894c6974f21f32995bcac7e640
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-04 00:05:02 +0900

    feat : 문제리스트 - 제목

--- frontend/src/Components/ProbList.js
+++ frontend/src/Components/ProbList.js
@@ -1,24 +1,54 @@
 import * as React from "react";
-import { Grid, ListItem, ListItemText } from "@mui/material";
+import { Grid, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
+import Level3 from "@mui/icons-material/Filter3";
+import Level1 from "@mui/icons-material/Filter1";
+import Level2 from "@mui/icons-material/Filter2";
+import Link from "@mui/material/Link";
 
 const ProbList = ({ data, title }) => {
+  let icon = {
+    1: <Level1 />,
+    2: <Level2 />,
+    3: <Level3 />,
+  };
+
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Typography
-            sx={{ mt: 4, mb: 2 }}
-            variant="h6"
-            component="div"
-            align="left"
+        <Grid item xs={12} md={60}>
+          <div
+            style={{
+              display: "flex",
+              textAlign: "left",
+              alignItems: "center",
+              paddingTop: "50px",
+              paddingBottom: "15px",
+            }}
           >
-            {title}
-          </Typography>
+            <label
+              style={{
+                fontFamily: "NotoSans-Bold",
+                fontSize: "25px",
+              }}
+            >
+              {title}
+            </label>
+            <Link
+              href="#"
+              underline="hover"
+              style={{
+                paddingLeft: "30px",
+              }}
+            >
+              {"더보기"}
+            </Link>
+          </div>
           <List>
             {data.map((item) => (
               <ListItem>
+                <ListItemIcon>{icon[item.level]}</ListItemIcon>
                 <ListItemText
                   primary={item.workbook_title + " " + item.problem_num + "번"}
                   secondary={item.subject + " - " + item.chapter}
