7b34f8fecfe035ac82d2c9ca0b3e34318687cdf0
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-04 20:41:34 +0900

    feat : 문제 리스트 (문제 없을 경우)

--- frontend/src/Components/ProbList.js
+++ frontend/src/Components/ProbList.js
@@ -8,6 +8,7 @@ import Level2 from "@mui/icons-material/Filter2";
 import Gold from "../Assets/image/gold-medal.png";
 import Silver from "../Assets/image/silver-medal.png";
 import Bronze from "../Assets/image/bronze-medal.png";
+import Pencil from "../Assets/image/pencil.png";
 
 import Link from "@mui/material/Link";
 
@@ -50,19 +51,45 @@ const ProbList = ({ data, title }) => {
               {"더보기"}
             </Link>
           </div>
-          <List>
-            {data.slice(0, 10).map((item) => (
-              <ListItem>
-                <ListItemText
-                  primary={item.workbook_title + " " + item.problem_num + "번"}
-                  secondary={item.subject + " - " + item.chapter}
-                />
-                <ListItemIcon>
-                  <img src={icon[item.level]} width="30px" />
-                </ListItemIcon>
-              </ListItem>
-            ))}
-          </List>
+          {data.length != 0 ? (
+            <List>
+              {data.slice(0, 10).map((item) => (
+                <ListItem>
+                  <ListItemText
+                    primary={
+                      item.workbook_title + " " + item.problem_num + "번"
+                    }
+                    secondary={item.subject + " - " + item.chapter}
+                  />
+                  <ListItemIcon>
+                    <img src={icon[item.level]} width="30px" />
+                  </ListItemIcon>
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingTop: "100px",
+              }}
+            >
+              <img src={Pencil} width="100px" />
+              <label
+                style={{
+                  textAlign: "center",
+                  alignItems: "center",
+                  color: "#434343",
+                  paddingTop: "40px",
+                }}
+              >
+                충분한 데이터가 수집되지 않았습니다.
+              </label>
+            </div>
+          )}
         </Grid>
       </Grid>
     </>
