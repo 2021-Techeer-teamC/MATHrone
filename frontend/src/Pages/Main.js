206113e5a3b78fdaa4bb5427d001e5d0824d9802
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-14 16:31:03 +0900

    fix : default main

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -18,44 +18,12 @@ import Books from "./Books";
 import Workbook from "./BookDetail";
 import Rank from "./Rank";
 
-const sections = [
-  { title: "소개", url: "/info" },
-  { title: "교재", url: "/books" },
-  { title: "랭킹", url: "/rank" },
-  { title: "소개", url: "/info" },
-  { title: "교재", url: "/books" },
-  { title: "랭킹", url: "/rank" },
-];
 
-const theme = createTheme();
 
 export default function Main() {
   return (
-    <Router basename="Main">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Header title="MATHrone" sections={sections} />
-          {/*<Paper>*/}
-          {/*  <img*/}
-          {/*    src="https://images.unsplash.com/photo-1596495577886-d920f1fb7238?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"*/}
-          {/*    alt="test image"*/}
-          {/*  />*/}
-          {/*</Paper>*/}
-          <main>
-            <Routes>
-              <Route path="/info" exact element={<InfoPage />} />
-              <Route path="/books" exact element={<Books />} />
-              <Route path="/workbook" exact element={<Workbook />} />
-              <Route path="/rank" exact element={<Rank />} />
-            </Routes>
-          </main>
-        </Container>
-        <Footer
-          title="Footer"
-          description="Something here to give the footer a purpose!"
-        />
-      </ThemeProvider>
-    </Router>
+      <>
+        <h1>Main Page</h1>
+      </>
   );
 }
