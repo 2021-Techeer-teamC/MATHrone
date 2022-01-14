a78895f67cf26c93c9eed643941741f5ba745c21
YujeongLEE <dldbwjd24@ewhain.net>
2022-01-14 16:30:52 +0900

    fix : router app으로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,43 @@ import './App.css';
 import Main from './Pages/Main'
 import SignUp from './Pages/SignUp'
 import SignIn from './Pages/SignIn';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Header from "./Components/Header";
+import InfoPage from "./Pages/InfoPage";
+import Books from "./Pages/Books";
+import Workbook from "./Pages/BookDetail";
+import Rank from "./Pages/Rank";
+import Footer from "./Components/Footer";
+import * as React from "react";
+
+const sections = [
+    { title: "소개", url: "/info" },
+    { title: "교재", url: "/books" },
+    { title: "랭킹", url: "/rank" },
+    { title: "소개", url: "/info" },
+    { title: "교재", url: "/books" },
+    { title: "랭킹", url: "/rank" },
+];
+
+const theme = createTheme();
+
+
 
 function App() {
   return (
     <div className="App">
-      <Main></Main>
-      <SignUp></SignUp>
-      <SignIn></SignIn>
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<Main/>}/>
+                <Route path="/info" exact element={<InfoPage />} />
+                <Route path="/books" exact element={<Books />} />
+                <Route path="/workbook" exact element={<Workbook />} />
+                <Route path="/rank" exact element={<Rank />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
