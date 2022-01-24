2a7838cd6a23589c906dde0e143021475fa9c2cb
h01010 <seoyeonju1198@gmail.com>
2022-01-25 01:03:41 +0900

    (MP-38) fix: 최상위 router 이동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 import Main from './Pages/Main'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import InfoPage from "./Pages/InfoPage";
+import Books from "./Pages/Books";
+import Workbook from "./Pages/BookDetail";
+import Rank from "./Pages/Rank";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <Main></Main>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            <Route path="/" exact element={<Main />} />
+            <Route path="/login" exact element={<SignIn />} />
+            <Route path="/info" exact element={<InfoPage />} />
+            <Route path="/books" exact element={<Books />} />
+            <Route path="/workbook" exact element={<Workbook />} />
+            <Route path="/rank" exact element={<Rank />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -5,16 +5,9 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-
 import Header from "../Components/Header";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Redirect,
-} from "react-router-dom";
 
 import InfoPage from "./InfoPage";
 import Books from "./Books";
@@ -33,33 +26,14 @@ const theme = createTheme();
 
 export default function Main() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header title="MATHrone" />
-        <NavBar sections={sections}/>
-        <Container maxWidth="lg">
-          {/*<Paper>*/}
-          {/*  <img*/}
-          {/*    src="https://images.unsplash.com/photo-1596495577886-d920f1fb7238?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"*/}
-          {/*    alt="test image"*/}
-          {/*  />*/}
-          {/*</Paper>*/}
-          <main>
-            <Routes>
-              <Route path="/login" exact element={<SignIn />} />
-              <Route path="/info" exact element={<InfoPage />} />
-              <Route path="/books" exact element={<Books />} />
-              <Route path="/workbook" exact element={<Workbook />} />
-              <Route path="/rank" exact element={<Rank />} />
-            </Routes>
-          </main>
-        </Container>
-        <Footer
-          title="Footer"
-          description="Something here to give the footer a purpose!"
-        />
-      </ThemeProvider>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header title="MATHrone" />
+      <NavBar sections={sections} />
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </ThemeProvider>
   );
 }
