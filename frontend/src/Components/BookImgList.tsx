6b44a485a2795b786bb70efe744e2219e4e354cb
YujeongLEE <dldbwjd24@ewhain.net>
2022-02-10 21:34:40 -0800

    fixed : fixed name

--- frontend/src/Components/BookImgList.tsx
+++ frontend/src/Components/BookImgList.tsx
@@ -3,16 +3,8 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import Paper from "@mui/material/Paper";
+import bookItem from "../Types/bookItem";
 
-//타입 정의
-interface bookItem {
-  workbook_id: string;
-  title: string;
-  img: string;
-  publisher: string;
-  level: number;
-  like: number;
-}
 
 //props로 전달받은 값들
 type BookListProps = {
@@ -20,7 +12,7 @@ type BookListProps = {
 }
 
 // {}로 감싸주지 않으면 posts: bookItem[]을 인식하지 못함
-const BookList = ( {posts} : BookListProps ) => {
+const BookImgList = ({posts} : BookListProps ) => {
   return (
     <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={10}>
       {posts.map((item:bookItem) => (
@@ -42,4 +34,4 @@ const BookList = ( {posts} : BookListProps ) => {
   );
 };
 
-export default BookList;
+export default BookImgList;
