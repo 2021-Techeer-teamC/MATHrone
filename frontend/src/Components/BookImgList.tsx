75f466884d9228c3862d237cec0bea76caacf9b8
YujeongLEE <dldbwjd24@ewhain.net>
2022-02-23 22:06:16 -0800

    feat : res parameter 이름 수정

--- frontend/src/Components/BookImgList.tsx
+++ frontend/src/Components/BookImgList.tsx
@@ -16,12 +16,12 @@ const BookImgList = ({posts} : BookListProps ) => {
   return (
     <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={10}>
       {posts.map((item:bookItem) => (
-        <ImageListItem key={item.img}>
+        <ImageListItem key={item.workbookId}>
           <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
+              src={`${item.profileImg}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.profileImg}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
           />
           <ImageListItemBar
             title={item.title}
