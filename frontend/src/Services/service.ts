c60af332f3f7d6d0585e5be3041a997c3fc15cc6
YujeongLEE <dldbwjd24@ewhain.net>
2022-02-23 22:30:49 -0800

    feat : 22/02/23 api 수정

--- frontend/src/Services/service.ts
+++ frontend/src/Services/service.ts
@@ -8,12 +8,12 @@ class DataService{
 //Books.txs
 
     //1. 출판사를 보내면 출판사 별 문제집을 반환
-    getWorkbookByPb(publisher : string){
-        return http.get<bookItem[]>(`/workbook?publisher=${publisher}`);//url+/publisher
+    getWorkbook(publisher : string, sortType: string, pageNum: number, category: string){
+        return http.get<bookItem[]>(`/workbook?publisher=${publisher}&sortType=${sortType}&category=${category}&pageNum=${pageNum}`);//url+/publisher
     }
 
     //2.모든 문제집을 반환
-    getAllWorkbook(){
+    getWorkbookList(){
         return http.get<bookItem[]>("/workbook");
     }
 
