1c479c2c2ba901e5472106c136cc7694d7380724
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-12 15:33:28 -0800

    feat : WorkbookService
    publisher=all category=all not all3가지경우

---
+++ backend/src/main/java/mathrone/backend/service/WorkBookServiceImpl.java
@@ -0,0 +1,45 @@
+package mathrone.backend.service;
+
+import mathrone.backend.domain.Problem;
+import mathrone.backend.domain.WorkBookInfo;
+import mathrone.backend.repository.ProblemRepository;
+import mathrone.backend.repository.WorkBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkBookServiceImpl implements WorkbookService{
+
+    //레포지토리 -> db에 접근
+    private final WorkBookRepository workBookRepository;
+
+
+    //생성자
+    @Autowired
+    public WorkBookServiceImpl(WorkBookRepository workBookRepository){
+        this.workBookRepository = workBookRepository;
+    }
+
+
+    @Override
+    public  List<WorkBookInfo> findWorkbook(String publisher, String category) {
+        if (publisher.equals("all"))
+            return workBookRepository.findAll();
+        else if(category.equals("all"))
+            return workBookRepository.findByPublisher(publisher);
+        else
+            return workBookRepository.findByPublisherAndCategory(publisher, category);
+    }
+
+
+    //workbook service와 problemservice가 왜 함께 있는지..!?
+//    private final ProblemRepository problemRepository;
+
+//    public List<Problem> findProblem(String workbookId, String chapterId){
+//        return problemRepository.findByWorkbookIdAndChapterId(workbookId, chapterId);
+//    }
+
+}
