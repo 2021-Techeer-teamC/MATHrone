5cc36c5e51a839c898527d3ae2cd6f60ad0a24ac
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-12 15:34:01 -0800

    feat : WorkbookRepository
    3가지 JPA Repo

--- backend/src/main/java/mathrone/backend/repository/WorkBookRepository.java
+++ backend/src/main/java/mathrone/backend/repository/WorkBookRepository.java
@@ -8,5 +8,13 @@ import java.util.List;
 
 @Repository
 public interface WorkBookRepository extends JpaRepository<WorkBookInfo, Long>{
+
+    //전체 문제집
+    List<WorkBookInfo> findAll();
+
+    //publisher를 이용하여 workbook을 찾는 기능 -> category가 관계(all) 없는 경우
     List<WorkBookInfo> findByPublisher(String publisher);
+
+    //publisher와 category를 이용하여 workbook을 찾는 기능 -> Workbook Info 타입의 리스트를 반환
+    List<WorkBookInfo> findByPublisherAndCategory(String publisher, String category);
 }
