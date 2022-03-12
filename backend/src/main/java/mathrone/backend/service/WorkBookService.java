0a42034c6bee4a9f4eb598184067d4848a092ea8
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-12 15:32:59 -0800

    feat : WorkbookService Impl로 의존성변경

--- backend/src/main/java/mathrone/backend/service/WorkBookService.java
+++ backend/src/main/java/mathrone/backend/service/WorkBookService.java
@@ -1,37 +1,11 @@
 package mathrone.backend.service;
 
-import mathrone.backend.domain.Problem;
 import mathrone.backend.domain.WorkBookInfo;
-import mathrone.backend.repository.ProblemRepository;
-import mathrone.backend.repository.WorkBookRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class WorkBookService {
+public interface WorkbookService {
 
-    private final WorkBookRepository workBookRepository;
-    private final ProblemRepository problemRepository;
-
-    public WorkBookService(WorkBookRepository workBookRepository, ProblemRepository problemRepository){
-        this.workBookRepository = workBookRepository;
-        this.problemRepository = problemRepository;
-    }
-
-    public void add(WorkBookInfo workBookInfo) {
-        workBookRepository.save(workBookInfo);
-    }
-
-    public  List<WorkBookInfo> findPublisher(String publisher) {
-        if (publisher.equals("all"))
-            return workBookRepository.findAll();
-        else
-            return workBookRepository.findByPublisher(publisher);
-    }
-
-    public List<Problem> findProblem(String workbookId, String chapterId){
-        return problemRepository.findByWorkbookIdAndChapterId(workbookId, chapterId);
-    }
+    public List<WorkBookInfo> findWorkbook(String publisher, String category);
 
 }
