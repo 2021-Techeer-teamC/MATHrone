3aec5614407909b24a99e37c19dd4e48f2567879
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-12 15:32:30 -0800

    feat : WorkbookController param추가
    param 4가지 추가

--- backend/src/main/java/mathrone/backend/controller/WorkbookController.java
+++ backend/src/main/java/mathrone/backend/controller/WorkbookController.java
@@ -2,7 +2,7 @@ package mathrone.backend.controller;
 
 import mathrone.backend.domain.Problem;
 import mathrone.backend.domain.WorkBookInfo;
-import mathrone.backend.service.WorkBookService;
+import mathrone.backend.service.WorkBookServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,21 +10,24 @@ import java.util.List;
 @RestController
 public class WorkbookController {
 
-    private final WorkBookService workBookService;
+    private final WorkBookServiceImpl workBookService;
 
-    public WorkbookController(WorkBookService workBookService){
+    public WorkbookController(WorkBookServiceImpl workBookService){
         this.workBookService = workBookService;
     }
 
     @GetMapping("/workbook") // 모든 워크북 조회(Books page)
-    public List<WorkBookInfo> bookList(@RequestParam(value="publisher", required = false, defaultValue = "all") String publisher){
-        return workBookService.findPublisher(publisher);
+    public List<WorkBookInfo> bookList(@RequestParam(value="publisher", required = false, defaultValue = "all") String publisher,
+                                       @RequestParam(value="sortType", required = false, defaultValue = "star") String sortType,
+                                       @RequestParam(value="pageNum", required = false, defaultValue = "1") int pageNum,
+                                       @RequestParam(value="category", required = false, defaultValue = "all") String category){
+        return workBookService.findWorkbook(publisher,category);
     }
 
-    @GetMapping("/problems") // 모든 문제 조회(Books page)
-    public List<Problem> problemList(@RequestParam(value="workbookId") String workbookId,
-                                     @RequestParam(value="chapterId") String chapterId){
-        return workBookService.findProblem(workbookId,chapterId);
-    }
+//    @GetMapping("/problems") // 모든 문제 조회(Books page)
+//    public List<Problem> problemList(@RequestParam(value="workbookId") String workbookId,
+//                                     @RequestParam(value="chapterId") String chapterId){
+//        return workBookService.findProblem(workbookId,chapterId);
+//    }
 
 }
