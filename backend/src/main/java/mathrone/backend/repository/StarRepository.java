b74e54925bb787eee30d18c79c33146b57f6c61f
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-14 21:58:54 -0700

    feat : (Repo) star count jpa

--- backend/src/main/java/mathrone/backend/repository/StarRepository.java
+++ backend/src/main/java/mathrone/backend/repository/StarRepository.java
@@ -2,11 +2,12 @@ package mathrone.backend.repository;
 
 import mathrone.backend.domain.Problem;
 import mathrone.backend.domain.StarInfo;
+import mathrone.backend.domain.WorkbookLevelInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StarRepository extends JpaRepository<StarInfo, Long> {
 
     //결과의 수 반환
-//    Long countByWorkbookId(Short workbookId);
+    Long countByWorkbookId(String workbookId);
 
 }
