3380b095396d7f381b99956e8ac4d42a750b7cf5
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-14 21:44:18 -0700

    feat : (Service) bookItem에 Level정보 추가

--- backend/src/main/java/mathrone/backend/service/WorkBookServiceImpl.java
+++ backend/src/main/java/mathrone/backend/service/WorkBookServiceImpl.java
@@ -1,11 +1,11 @@
 package mathrone.backend.service;
 
-import mathrone.backend.domain.Problem;
 import mathrone.backend.domain.WorkBookInfo;
-import mathrone.backend.repository.ProblemRepository;
+import mathrone.backend.domain.WorkbookLevelInfo;
+import mathrone.backend.repository.LevelRepository;
+import mathrone.backend.repository.StarRepository;
 import mathrone.backend.repository.WorkBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +15,13 @@ public class WorkBookServiceImpl implements WorkbookService{
 
     //레포지토리 -> db에 접근
     private final WorkBookRepository workBookRepository;
-
+    private final LevelRepository levelRepository;
 
     //생성자
     @Autowired
-    public WorkBookServiceImpl(WorkBookRepository workBookRepository){
+    public WorkBookServiceImpl(WorkBookRepository workBookRepository, LevelRepository levelRepository){
         this.workBookRepository = workBookRepository;
+        this.levelRepository = levelRepository;
     }
 
 
@@ -43,6 +44,31 @@ public class WorkBookServiceImpl implements WorkbookService{
         else
             return workBookRepository.countByPublisherAndCategory(publisher, category);
     }
+
+    @Override
+    public String getLevel(String workbookId){
+        //해당 문제집의 레벨투표 정보를 가져옴
+        WorkbookLevelInfo wb = levelRepository.findByWorkbookId(workbookId);
+
+        //각 난이도별 투표수
+        int high = wb.getHighCnt();
+        int mid = wb.getMidCnt();
+        int low = wb.getLowCnt();
+
+        //투표수중 최대값
+        int maxValue = Math.max(high,Math.max(mid,low));
+
+        if(maxValue==low) return "1";
+        else if(maxValue==mid) return "2";
+        else return "3";
+
+    }
+
+//    @Override
+//    public Long getLike(String workbookId){
+//        return StarRepository.countByWorkbookId(workbookId);
+//    }
+
 
 
 
