6ce3a944961be7b3b8af41cca58bf1d7f2bfaadf
ohhondgi <5ghdrl@naver.com>
2022-03-17 16:03:51 +0900

    chore: fix swagger error

--- backend/src/main/java/mathrone/backend/config/SecurityConfig.java
+++ backend/src/main/java/mathrone/backend/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/user/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
