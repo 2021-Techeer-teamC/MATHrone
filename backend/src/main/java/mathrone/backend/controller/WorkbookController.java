e18d0ad16d6192e8100d003692dd65added250ea
YujeongLEE <dldbwjd24@ewhain.net>
2022-03-17 01:39:05 -0700

    feat : (Left Nav bar - Cont) test for Service

--- backend/src/main/java/mathrone/backend/controller/WorkbookController.java
+++ backend/src/main/java/mathrone/backend/controller/WorkbookController.java
@@ -1,13 +1,16 @@
 package mathrone.backend.controller;
 
 import mathrone.backend.domain.Problem;
+import mathrone.backend.domain.PubCatPair;
 import mathrone.backend.domain.WorkBookInfo;
 import mathrone.backend.domain.bookItem;
 import mathrone.backend.service.WorkBookServiceImpl;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WorkbookController {
@@ -21,9 +24,30 @@ public class WorkbookController {
 
     @GetMapping("/workbook") // 모든 워크북 조회(Books page)
     public List<bookItem> bookList(@RequestParam(value="publisher", required = false, defaultValue = "all") String publisher,
-                             @RequestParam(value="sortType", required = false, defaultValue = "star") String sortType,
-                             @RequestParam(value="pageNum", required = false, defaultValue = "1") int pageNum,
-                             @RequestParam(value="category", required = false, defaultValue = "all") String category){
+                                      @RequestParam(value="sortType", required = false, defaultValue = "star") String sortType,
+                                      @RequestParam(value="pageNum", required = false, defaultValue = "1") int pageNum,
+                                      @RequestParam(value="category", required = false, defaultValue = "all") String category){
+
+
+//        List<bookContent> contentList = new ArrayList<bookContent>();
+        System.out.println("================================");
+        List<PubCatPair> ress = workBookService.getPublisherAndCategoryList();
+        System.out.println(ress);
+        System.out.println("================================");
+        for (PubCatPair wb: ress) {
+            System.out.println("================================");
+            //이미 존재하는 pulisher
+            String p = wb.getPublisher();
+            String c = wb.getCategory();
+            System.out.println("==================");
+            System.out.println(p);
+            System.out.println(c);
+            System.out.println("================================");
+            //else
+
+        }
+//
+//        return contentList;
 
 
         //결과로 반환할 bookItem 리스트 (임시)
