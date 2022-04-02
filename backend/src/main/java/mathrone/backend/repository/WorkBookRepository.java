2e91abe7598867a243fd6d9b40dcb0b357ea7332
hyunwoo <ashyonu@gmail.com>
2022-04-02 17:05:41 +0900

    refactor : seperate workbook, userinfo repository
    workbook 레포에 맞지 않던 유저 정보 조회 구문 분리

--- backend/src/main/java/mathrone/backend/repository/WorkBookRepository.java
+++ backend/src/main/java/mathrone/backend/repository/WorkBookRepository.java
@@ -11,12 +11,6 @@ import java.util.List;
 public interface WorkBookRepository extends JpaRepository<WorkBookInfo, Long>{
     List<WorkBookInfo> findByPublisher(String publisher);
 
-    //@Query 어노테이션으로 직접 쿼리 작성
-    @Query(value = "SELECT nickname FROM user_info WHERE user_id=:user_id", nativeQuery = true)
-    String getNickname(int user_id);
-    // user_id를 통해서 user_nickname 조회
 
-    @Query(value = "SELECT COUNT(*) FROM problem_try WHERE user_id=:user_id GROUP BY user_id", nativeQuery = true)
-    int getTryByUserID(int user_id);
     // user_id에 따라서 시도한 문제의 수를 조회하는 함수
 }
