24ca96fabeb1ccb79fc62ac8843bd9578030a8ea
YujeongLEE <dldbwjd24@ewhain.net>
2022-04-12 12:57:06 -0700

    feat : workbook api 수정

--- frontend/src/Services/service.ts
+++ frontend/src/Services/service.ts
@@ -7,19 +7,19 @@ class DataService{
 
 //Books.txs
 
-    //1. 출판사를 보내면 출판사 별 문제집을 반환
+    //1. 출판사/정렬기준/category/pageNum를 보내면 출판사 별 문제집을 반환 (9개의 결과)
     getWorkbook(publisher : string, sortType: string, pageNum: number, category: string){
         return http.get<bookItem[]>(`/workbook?publisher=${publisher}&sortType=${sortType}&category=${category}&pageNum=${pageNum}`);//url+/publisher
     }
 
-    //2.모든 문제집을 반환
-    getWorkbookList(){
-        return http.get<bookItem[]>("/workbook");
+    //2. 출판사/category별 문제의 총 갯수
+    getWorkbookInfo(publisher: string, category: string){
+        return http.get<bookItem[]>(`/workbook/info?publisher=${publisher}&category=${category}`);
     }
 
     //3. 문제집 리스트를 반환
-    getAllBookContent(){
-        return http.get<bookContent[]>("");
+    getWorkbookList(){
+        return http.get<bookContent[]>("/workbook/list");
     }
 }
 
