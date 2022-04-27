8a6e5c780b911a0da4942dcc0d250c0f4b4ea8b3
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2022-04-27 15:20:58 -0700

    (MR-34) workbook page에서 workbook detail 페이지로의 라우팅 연결

--- frontend/src/Components/BookImgList.tsx
+++ frontend/src/Components/BookImgList.tsx
@@ -1,34 +1,36 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import Paper from "@mui/material/Paper";
 import bookItem from "../Types/bookItem";
 
-
 //props로 전달받은 값들
 type BookListProps = {
-  posts : bookItem[];
-}
+  posts: bookItem[];
+};
 
 // {}로 감싸주지 않으면 posts: bookItem[]을 인식하지 못함
-const BookImgList = ({posts} : BookListProps ) => {
+const BookImgList = ({ posts }: BookListProps) => {
   return (
     <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={10}>
-      {posts.map((item:bookItem) => (
-        <ImageListItem key={item.workbookId}>
-          <img
+      {posts.map((item: bookItem) => (
+        <Link to={`/books/${item.workbookId}`}>
+          <ImageListItem key={item.workbookId}>
+            <img
               src={`${item.profileImg}?w=248&fit=crop&auto=format`}
               srcSet={`${item.profileImg}?w=248&fit=crop&auto=format&dpr=2 2x`}
               alt={item.title}
               loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>{item.publisher}</span>}
-            position="below"
-          />
-        </ImageListItem>
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={<span>{item.publisher}</span>}
+              position="below"
+            />
+          </ImageListItem>
+        </Link>
       ))}
     </ImageList>
   );
