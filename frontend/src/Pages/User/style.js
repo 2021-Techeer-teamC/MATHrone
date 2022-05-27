a1b212b9a675636d57f27d9b45e0f54142a121ee
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2022-05-26 20:58:49 -0700

    (MP-47) 로그인 화면 디자인 변경

---
+++ frontend/src/Pages/User/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SignInDiv = styled.div`
+  background: red;
+  #login_button {
+    background: #315C72;
+    font: red;
+    :hover {
+      background: #009688;
+    }
+  }
+`;
