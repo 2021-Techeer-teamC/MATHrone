1c3ec3cb109b3f769ad3ca83418f2c7f69291a58
YujeongLEE <dldbwjd24@ewhain.net>
2022-05-26 17:56:07 -0700

    feat : (controller) user star book

--- backend/src/main/java/mathrone/backend/controller/MainPageController.java
+++ backend/src/main/java/mathrone/backend/controller/MainPageController.java
@@ -1,8 +1,10 @@
 package mathrone.backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import mathrone.backend.domain.userWorkbookData;
 import mathrone.backend.service.MainPageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +22,10 @@ public class MainPageController {
     @GetMapping("/main/workbook/try") // 상위 랭킹 정보를 가져옴
     public List<userWorkbookData> gettryinglist(){
         return mainPageService.getTryingBook(1);
+    }
+
+    @GetMapping("/main/workbook/star")
+    public List<userWorkbookData> getStarList(@RequestParam(value="userId", required = false) Integer userId){
+        return mainPageService.getStarBook(userId);
     }
 }
