1ffdb938333e14315138ffd01cce619155875864
hyunwoo <ashyonu@gmail.com>
2022-06-08 22:03:17 +0900

    feat : change to horizontal alignment
    add scroll to table

--- frontend/src/Pages/Result.tsx
+++ frontend/src/Pages/Result.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Header from "../Components/Header";
-import NavBar from "../Components/NavBar";
-import Footer from "../Components/Footer";
 import { Box } from "@mui/system";
 import Card from '@mui/material/Card';
-import {Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
+import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import Chart from "../Components/Chart";
 
 interface answerData {
@@ -14,9 +12,9 @@ interface answerData {
 }
 
 interface score {
-    title : string;
-    value : number;
-    color : string;
+    title: string;
+    value: number;
+    color: string;
 }
 
 let count: number;
@@ -38,6 +36,124 @@ const answerData: answerData[] = [
         problem_id: "01-01-00003",
         my_answer: 3,
         answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    }, {
+        problem_id: "01-01-00001",
+        my_answer: 1,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
+    },
+    {
+        problem_id: "01-01-00002",
+        my_answer: 2,
+        answer: 1,
+    },
+    {
+        problem_id: "01-01-00003",
+        my_answer: 3,
+        answer: 3,
     },
 ]
 
@@ -46,9 +162,8 @@ export default function Result(props: { sections: any; }) {
     correct = 0;
     wrong = 0;
 
-    answerData.map((answerData: answerData) => 
-        {answerData.my_answer === answerData.answer ? correct++: wrong++})
-    
+    answerData.map((answerData: answerData) => { answerData.my_answer === answerData.answer ? correct++ : wrong++ })
+
 
     var score: score[] = [
         { title: 'correct', value: correct, color: '#73C23A' },
@@ -58,47 +173,53 @@ export default function Result(props: { sections: any; }) {
     return (
         <div>
             <Header title="MATHrone" sections={props.sections} />
-            <Box sx={{ display: 'block' }}>
-                <Box sx={{ display: 'flex', justifyContent: 'center', margin: '3%' }}>
-                    <Chart data={score}/>
-                </Box>
-                <Table style={{ width: '50%', margin: 'auto' }}>
-                    <TableHead>
-                        <TableRow style={{ backgroundColor: '#d2d2d2' }}>
-                            <TableCell align="center" padding='none'> No. </TableCell>
-                            <TableCell align="center" padding='none'> 정 답 </TableCell>
-                            <TableCell align="center" padding='none'> 나의 답 </TableCell>
-                            <TableCell align="center" padding='none'> 결 과 </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {answerData.map((answerData: answerData) =>
-                        (
-                            <TableRow key={answerData.problem_id}>
-                                <TableCell align="center" padding='none'>
-                                    {++count}
-                                </TableCell>
-                                <TableCell align="center" padding='none'>
-                                    {answerData.answer}
-                                </TableCell>
-                                <TableCell align="center" padding='none'>
-                                    {answerData.my_answer}
-                                </TableCell>
-                                <TableCell align="center" padding='none'>
-                                    <Box>
-                                        <Box sx={{ display: "flex", justifyContent: "center" }}>
-                                            {answerData.my_answer === answerData.answer ?
-                                                <Card sx={{ backgroundColor: '#73C23A', p: 'none', width: '50%' }}><Typography>정답</Typography></Card> :
-                                                <Card sx={{ backgroundColor: '#C13C37', p: 'none', width: '50%' }}><Typography>오답</Typography></Card>
-                                            }
-                                        </Box>
-                                    </Box>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Box>
+            <Grid container spacing={1} justifyContent="center" alignItems="center">
+                <Grid item xs={6}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', margin: '3%' }}>
+                        <Chart data={score} />
+                    </Box>
+                </Grid>
+                <Grid item xs={6} justifyContent="left">
+                    <Box ml={2} sx={{ width: '90%', overflow: 'hidden' }}>
+                        <TableContainer sx={{ maxHeight: 600 }}>
+                            <Table stickyHeader aria-label="sticky table" >
+                                <TableHead>
+                                    <TableRow style={{ backgroundColor: '#d2d2d2' }}>
+                                        <TableCell align="center" padding='none'> No. </TableCell>
+                                        <TableCell align="center" padding='none'> 정 답 </TableCell>
+                                        <TableCell align="center" padding='none'> 나의 답 </TableCell>
+                                        <TableCell align="center" padding='none'> 결 과 </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {answerData.map((answerData: answerData) =>
+                                    (
+                                        <TableRow key={answerData.problem_id}>
+                                            <TableCell align="center" padding='none'>
+                                                {++count}
+                                            </TableCell>
+                                            <TableCell align="center" padding='none'>
+                                                {answerData.answer}
+                                            </TableCell>
+                                            <TableCell align="center" padding='none'>
+                                                {answerData.my_answer}
+                                            </TableCell>
+                                            <TableCell align="center" padding='none'>
+                                                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                                                    {answerData.my_answer === answerData.answer ?
+                                                        <Card sx={{ backgroundColor: '#73C23A', p: 'none', width: '50%' }}><Typography>정답</Typography></Card> :
+                                                        <Card sx={{ backgroundColor: '#C13C37', p: 'none', width: '50%' }}><Typography>오답</Typography></Card>
+                                                    }
+                                                </Box>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
     );
 }
