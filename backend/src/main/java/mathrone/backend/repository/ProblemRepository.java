0628de73e66b1eaf2c68bcc9d0b9e43cacf045d2
hyunwoo <ashyonu@gmail.com>
2022-07-07 00:00:07 +0900

    feat : update Query

--- backend/src/main/java/mathrone/backend/repository/ProblemRepository.java
+++ backend/src/main/java/mathrone/backend/repository/ProblemRepository.java
@@ -13,6 +13,6 @@ public interface ProblemRepository extends JpaRepository<Problem, Long> {
 
     Problem findByProblemId(String problemId);
 
-    @Query(value = "SELECT * FROM problem WHERE problem_id in (SELECT problem_id FROM problem_try order by date asc limit 20) limit 10", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT ON (problem_id) * FROM problem WHERE problem_id in (SELECT problem_id FROM problem_try order by created_at asc) limit 10", nativeQuery = true)
     List<Problem> findByRecentTry(); // 가장 최근에 시도한 문제 10개 반환
 }
