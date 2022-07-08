5c35b55192904f70dcc784821292be8fc68e5c78
OhHongGi <5ghdrl@gmail.com>
2022-07-08 16:02:12 +0900

    chore: Insert tried problem data after problem scoring

--- backend/src/main/java/mathrone/backend/service/AnswerServiceImpl.java
+++ backend/src/main/java/mathrone/backend/service/AnswerServiceImpl.java
@@ -2,12 +2,19 @@ package mathrone.backend.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import mathrone.backend.controller.dto.ProblemGradeRequestDto;
 import mathrone.backend.controller.dto.ProblemGradeResponseDto;
+import mathrone.backend.domain.Problem;
+import mathrone.backend.domain.ProblemTry;
 import mathrone.backend.domain.Solution;
+import mathrone.backend.domain.UserInfo;
+import mathrone.backend.repository.ProblemRepository;
+import mathrone.backend.repository.ProblemTryRespository;
 import mathrone.backend.repository.SolutionRepository;
+import mathrone.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +22,46 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AnswerServiceImpl implements AnswerService {
 
-    @Autowired
     private final SolutionRepository solutionRepository;
+    private final ProblemRepository problemRepository;
+    private final UserRepository userRepository;
+    private final ProblemTryRespository problemTryRespository;
 
     @Transactional
     public List<ProblemGradeResponseDto> gradeProblem(
         ProblemGradeRequestDto problemGradeRequestDtoList) {
         List<ProblemGradeResponseDto> problemGradeResponseDtoList = new ArrayList<>();
         List<ProblemGradeRequestDto.problemSolve> list = problemGradeRequestDtoList.getAnswerSubmitList();
+        UserInfo user = userRepository.findById(problemGradeRequestDtoList.getUserId()).get();
+
         for (ProblemGradeRequestDto.problemSolve problem : list) {
             Solution solutionProblem = solutionRepository.findSolutionByProblemId(
                 problem.getProblemId());
+            Problem registedProblem = problemRepository.findByProblemId(problem.getProblemId());
             boolean isCorrect = false;
             if (solutionProblem.getAnswer() == problem.getSolution()) {
                 isCorrect = true;
             }
+
+            Optional<ProblemTry> registedProblemTry = problemTryRespository.findAllByProblemAndUser(registedProblem,
+                user);
+            if (registedProblemTry.isPresent()){
+                ProblemTry problemTry = registedProblemTry.get();
+                problemTry.setIscorrect(isCorrect);
+                problemTry.setAnswerSubmitted(problem.getSolution());
+            } else {
+                ProblemTry problemTry = ProblemTry.builder()
+                    .answerSubmitted(problem.getSolution())
+                    .iscorrect(isCorrect)
+                    .user(user)
+                    .problem(registedProblem)
+                    .build();
+
+//                user.getProblemTryList().add(problemTry);
+//                registedProblem.getProblemTryList().add(problemTry);
+                problemTryRespository.save(problemTry);
+            }
+
             problemGradeResponseDtoList.add(ProblemGradeResponseDto.builder()
                 .problemId(problem.getProblemId())
                 .solution(problem.getSolution())
