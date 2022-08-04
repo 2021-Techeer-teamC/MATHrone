4f19670af0307992bf081d2240303484ce6ceee6
YujeongLEE <dldbwjd24@ewhain.net>
2022-08-04 19:32:41 +0900

    feat : DTO 형태로 domain 수정
    Entity가 아니다 주의

--- backend/src/main/java/mathrone/backend/domain/UserProfile.java
+++ backend/src/main/java/mathrone/backend/domain/UserProfile.java
@@ -2,12 +2,13 @@ package mathrone.backend.domain;
 
 import com.sun.istack.NotNull;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
-@Entity
+@Getter
 public class UserProfile {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //JPA 사용시 필요)
@@ -39,10 +40,10 @@ public class UserProfile {
 
     private String role;
 
-//    private UserRank rankInfo;
+    private UserRank rankInfo;
 
 
-    public UserProfile(int userId, String id, String password, String profileImg, int exp, boolean premium, String email, String phoneNum, String userImg, String role) {
+    public UserProfile(int userId, String id, String password, String profileImg, int exp, boolean premium, String email, String phoneNum, String userImg, String role, UserRank rankInfo) {
         this.userId = userId;
         this.id = id;
         this.password = password;
@@ -53,87 +54,7 @@ public class UserProfile {
         this.phoneNum = phoneNum;
         this.userImg = userImg;
         this.role = role;
+        this.rankInfo = rankInfo;
     }
 
-
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getProfileImg() {
-        return profileImg;
-    }
-
-    public void setProfileImg(String profileImg) {
-        this.profileImg = profileImg;
-    }
-
-    public int getExp() {
-        return exp;
-    }
-
-    public void setExp(int exp) {
-        this.exp = exp;
-    }
-
-    public boolean isPremium() {
-        return premium;
-    }
-
-    public void setPremium(boolean premium) {
-        this.premium = premium;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    public String getUserImg() {
-        return userImg;
-    }
-
-    public void setUserImg(String userImg) {
-        this.userImg = userImg;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
