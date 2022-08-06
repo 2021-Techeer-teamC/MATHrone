80c9304582841930364e08ae118294855ef056a7
YujeongLEE <dldbwjd24@ewhain.net>
2022-08-06 22:29:37 +0900

    feat** : (Service) 일시 오류 userId관련으로 인해 수정(복구필요

--- backend/src/main/java/mathrone/backend/service/ProfileService.java
+++ backend/src/main/java/mathrone/backend/service/ProfileService.java
@@ -2,13 +2,19 @@ package mathrone.backend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.RequiredArgsConstructor;
+import mathrone.backend.controller.dto.TokenRequestDto;
 import mathrone.backend.domain.UserInfo;
 import mathrone.backend.domain.UserProfile;
 import mathrone.backend.domain.UserRank;
+import mathrone.backend.domain.token.LogoutAccessToken;
 import mathrone.backend.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+import mathrone.backend.util.TokenProviderUtil;
+import javax.transaction.Transactional;
 
 @Service
 public class ProfileService {
@@ -22,12 +28,12 @@ public class ProfileService {
     }
 
     //userId를 받아와서 전송
-    public UserProfile getProfile(Integer userId){
+    public UserProfile getProfile(String id){//이 부분 수정이 필요! (현재 userId가 pk가 아닌 id로 되어있어서 임시로 이렇게 찾는 방법으로 해둠)
 
         //유저 정보 받아오기
-        UserInfo userinfo = userInfoRepository.findByUserId(userId);
+        UserInfo userinfo = userInfoRepository.findById(id);
         //랭크 정보 받아오기
-        ObjectNode node = getMyRank(userId);
+        ObjectNode node = getMyRank(userinfo.getUserId());
 
         //랭크 정보를 DTO에 담기
         UserRank r = new UserRank(node.findValue("rank").toString(), node.findValue("score").toString(), node.findValue("try").toString());
@@ -35,7 +41,7 @@ public class ProfileService {
         //최종 Profile 생성
         UserProfile res = new UserProfile(userinfo.getUserId(), userinfo.getId(), userinfo.getPassword(), userinfo.getProfileImg(),userinfo.getExp(),
                 userinfo.isPremium(),userinfo.getEmail(), userinfo.getPhoneNum(), userinfo.getUserImg(),userinfo.getRole(),r);
-        
+
         return res;
     }
 
