35eb2b49cf3f80c8914df8ad4eeec8c5024b1456
YujeongLEE <dldbwjd24@ewhain.net>
2022-08-06 22:30:08 +0900

    feat : (Controller) Profile 정보조회

--- backend/src/main/java/mathrone/backend/controller/ProfileController.java
+++ backend/src/main/java/mathrone/backend/controller/ProfileController.java
@@ -1,29 +1,32 @@
 package mathrone.backend.controller;
 
+import mathrone.backend.controller.dto.TokenRequestDto;
 import mathrone.backend.domain.UserInfo;
 import mathrone.backend.domain.UserProfile;
+import mathrone.backend.service.AuthService;
 import mathrone.backend.service.MainPageService;
 import mathrone.backend.service.ProfileService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
 
     private final ProfileService profileService;
+    private final AuthService authService;
 
-    public ProfileController(ProfileService profileService) {
+    public ProfileController(ProfileService profileService, AuthService authService) {
         this.profileService = profileService;
+        this.authService = authService;
     }
+    
 
-    //http ~ /myprofile?userId={}
-    @GetMapping("/myprofile")
-    public UserProfile getProfile(@RequestParam(value="userId", required = false) Integer userId){
-        return profileService.getProfile(userId);
+    @RequestMapping(value = "/myprofile", method = RequestMethod.GET)
+    public UserProfile getProfile( @RequestHeader String accessToken,
+                                   @RequestHeader String refreshToken
+    ){
+        return profileService.getProfile(authService.getUserIdFromAT(new TokenRequestDto(accessToken,refreshToken)));
     }
 
 
